43c3bdff0de7414609794084b6adbf19df506256
BlueEyedHush <BlueEyedHush@users.noreply.github.com>
2017-12-18 10:46:20 -0500

    Templating for kubernetes deployment files (#2)
    * code for performing substitution on files
    
    * moving crud configuration to crud directory, renaming files
    
    * conceptual chagne: generic deploy.py file for all services
    
    * parsing vars from file
    
    * substituting inside config files; putting temp files in dedicated directory; cleaning up temp files
    
    * more readable paths of generated files; adding option to skip cleanup
    
    * putting variables instead of literals in template files
    
    * introducing $service variable

---
+++ deploy.py
@@ -0,0 +1,66 @@
+
+import os
+import sys
+import codecs
+import string
+import tempfile
+import json
+import shutil
+
+def get_script_dir():
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+def get_vars_dict(dir):
+    f = codecs.open(os.path.join(dir, "vars.json"))
+    vars_dict = json.loads(f.read())
+    f.close()
+    return vars_dict
+
+
+def substitute(mapping, in_dir, filename, out_dir):
+    '''
+
+    :param mapping: 
+    :param filename: 
+    :return: name of new file created by substituting $ references 
+    '''
+    in_path = os.path.join(in_dir, filename)
+    out_path = os.path.join(out_dir, filename)
+
+    template_file = codecs.open(in_path, 'r', 'utf-8')
+    template = template_file.read()
+    template_file.close()
+
+    content_with_substitutes = string.Template(template).substitute(mapping)
+
+    output_file = codecs.open(out_path, 'w', 'utf-8')
+    output_file.write(content_with_substitutes)
+    output_file.close()
+
+    return out_path
+
+
+def get_dict_to_substituted_files(original_config_dir, out_dir):
+    vars_dict = get_vars_dict(original_config_dir)
+
+    filename_pairs = [("config", "config.properties"), ("dep", "deployment.yml"), ("svc", "service.yml")]
+    substituted_pairs = map(lambda (k, fname): (k, substitute(vars_dict, original_config_dir, fname, out_dir)),
+                            filename_pairs)
+
+    return dict(substituted_pairs)
+
+
+if __name__ == "__main__":
+    service_name = sys.argv[1]
+    cleanup_temp = not bool(sys.argv[2])
+
+    config_dir = os.path.join(get_script_dir(), service_name)
+    out_dir = tempfile.mkdtemp(prefix="{}-".format(service_name))
+
+    substitued = get_dict_to_substituted_files(config_dir, out_dir)
+
+    if cleanup_temp:
+        shutil.rmtree(out_dir)
+    else:
+        print substitued
