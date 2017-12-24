9163da82f1ac8b6875ee6527e441e5046a3dcffd
knawara <blueeyedhush@gmail.com>
2017-12-24 13:28:02 +0100

    cluster machines have scope which allows access to all APIs - authorization performed using IAM mechanisms

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 gcloud container clusters create miosr \
-    --zone=europe-west3-a \
-    --project=arched-shuttle-189207 \
-    --num-nodes=3 \
-    --machine-type=g1-small
+    --zone europe-west3-a \
+    --project arched-shuttle-189207 \
+    --num-nodes 3 \
+    --machine-type g1-small \
+    --scopes cloud-platform
